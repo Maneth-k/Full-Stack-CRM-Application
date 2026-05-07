b9a7553bd34ad2badaa129080364ff4c228c20ab
Maneth-k <manethkaveen0@gmail.com>
2026-05-07 15:45:28 +0530

    adjusted cookie settings for the deployment

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -21,8 +21,8 @@ export const login = async (req: Request, res: Response) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      secure: true,
+      sameSite: 'none',
       maxAge: 86400000 // 1 day
     });
 
